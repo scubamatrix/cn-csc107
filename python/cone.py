547ac456435216daa3d3c442956821a66285d157
scubamatrix <scubamatrix@gmail.com>
2024-08-27 21:16:40 -0400

    Add header comments and delete file
    Add header comments  to cone.py
    Delete cone_calculator.py

--- python/cone.py
+++ python/cone.py
@@ -1,11 +1,21 @@
-# A program to calculate the surface area of a cone by Wonder Student
-# Load the math module to get pi and the sqrt function
+#!/usr/bin/env python3
+"""
+cone.py
+
+A program to calculate the surface area of a cone. 
+
+Author: Jeff Holmes
+Date: 01/06/2024
+Version: 1.0
+Python: 3.11.6
+"""
+# Load math module to get pi and the sqrt functions
 import math
 
 # Introduce the program
 print("This program determines the surface area of a cone.")
 
-# Get the Data
+# Get the input (data) from user
 height = float(input("Enter the height of the cone in inches: "))
 radius = float(input("Enter the radius of the cone in inches: "))
 
